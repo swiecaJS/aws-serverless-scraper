04c50d41f8dd4f64864b0d78f55bf39c5063f36f
Karol <swieca.karol@gmail.com>
2018-01-07 22:37:23 +0100

    working properly

--- handler.js
+++ handler.js
@@ -1,27 +1,72 @@
 'use strict';
 
 const request = require('axios');
-const {getCurrentWPVer} = require('./helper');
+const AWS = require('aws-sdk');
+const dynamo = new AWS.DynamoDB.DocumentClient();
+const Nexmo = require('nexmo')
+const { differenceWith, isEqual } = require('lodash');
+const { getCurrentWPVer } = require('./helper');
 
-module.exports.hello = (event, context, callback) => {
+module.exports.scrape = (event, context, callback) => {
 
-  // const response = {
-  //   statusCode: 200,
-  //   body: JSON.stringify({
-  //     message: 'Go Serverless v1.0! Your function executed successfully!',
-  //     input: event,
-  //   }),
-  // };
 
+let todayVer, dbVer, scrapedSameAsYestarday;
 
   request('https://wordpress.org/download/')
     .then(({data}) => {
-      const ver = getCurrentWPVer(data);
-      callback(null, {ver});
+      todayVer = getCurrentWPVer(data);
+
+      // get yesterday's WP ver
+      return dynamo.scan({
+        TableName: 'wpVer'
+      }).promise();
+
+    })
+    .then(dbResp => {
+
+      let yesterdayWpVer = dbResp.Items[0] ? dbResp.Items[0].ver : [];
+
+      scrapedSameAsYestarday = (todayVer == yesterdayWpVer)
+
+      if(scrapedSameAsYestarday) {
+        callback(null, {
+          response: 'nothing changed! Current ver is still: ' + todayVer
+        })
+      }
+      if (!scrapedSameAsYestarday) {
+        let recordToDelete = dbResp.Items[0] ? dbResp.Items[0].listingId : null
+
+        if(recordToDelete) {
+          return dynamo.delete({
+            TableName: 'wpVer',
+            Key: {
+              listingId: recordToDelete
+            }
+          }).promise()
+        } else return;
+      }
+    })
+    .then(() => {
+      if(!scrapedSameAsYestarday) {
+        return dynamo.put({
+          TableName: 'wpVer',
+          Item: {
+            listingId: new Date().toString(),
+            ver: todayVer
+          }
+        }).promise()
+      }
+    })
+    .then(() => {
+      if(!scrapedSameAsYestarday) {
+
+
+        callback(null, {
+          message: 'new version released!',
+          ver: todayVer
+        });
+      }
+
     })
     .catch(callback);
-  // callback(null, response);
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
 };
